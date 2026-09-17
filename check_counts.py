f92c57b29bddd43c314c0de63df637f3d748cd6f
shaurya212121 <shauryas2107@gmail.com>
2026-09-17 17:11:47 +0530

    chore: remove scratch file

--- check_counts.py
+++
@@ -1,8 +0,0 @@
-import os
-from supabase import create_client
-from dotenv import load_dotenv
-load_dotenv('backend/.env')
-supabase = create_client(os.environ.get('SUPABASE_URL'), os.environ.get('SUPABASE_SERVICE_KEY'))
-for t in ['oil_spills', 'drift_paths', 'suspect_vessels', 'vessels', 'vessel_positions', 'alerts']:
-    res = supabase.table(t).select('*', count='exact').limit(1).execute()
-    print(f'{t}: {res.count}')
